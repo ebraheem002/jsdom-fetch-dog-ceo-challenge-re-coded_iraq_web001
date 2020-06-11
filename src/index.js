c89d7c64867cfbe92fe38bfe2ab852469308cc86
Ebraheem Mahmood <ebraheemmahmood991@gmail.com>
2020-06-11 16:06:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('%c HI', 'color: firebrick')
+const URL1 = "https://dog.ceo/api/breeds/image/random/4"
+fetch(URL1)
+.then(response => console.console.log(response);)
+.then(json => {
+
+})
